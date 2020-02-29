ea864f626cdcdeb276767db2f55ae0c60cfc7689
Leonardo Jorge Tecco <teccoleo@hotmail.com>
2020-02-29 18:14:39 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import {SafeAreaView, StatusBar, Text} from 'react-native'
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>{'Concursar'}</Text>
+      </SafeAreaView>
+    </>
+  )
+}
+
+export default App
